52be0fd8247ebc5d23ff2cb35bf52cd3852ebd48
julianrolon <julianrolonramirez@gmail.com>
2022-10-11 13:33:09 -0500

    Initial SpringBoot Project

---
+++ src/main/java/tech/julkrolon/demoKafkaSpringBoot/DemoKafkaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package tech.julkrolon.demoKafkaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/julkrolon/demoKafkaSpringBoot/DemoKafkaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.julkrolon.demoKafkaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
