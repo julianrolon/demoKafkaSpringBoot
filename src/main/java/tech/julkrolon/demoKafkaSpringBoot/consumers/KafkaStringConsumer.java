e00171b699a9022b7d4dccefa7f5afdfaab5c1cf
julianrolon <julianrolonramirez@gmail.com>
2022-10-12 08:54:03 -0500

    Kafka Consumer

---
+++ src/main/java/tech/julkrolon/demoKafkaSpringBoot/consumers/KafkaStringConsumer.java
@@ -0,0 +1,16 @@
+package tech.julkrolon.demoKafkaSpringBoot.consumers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaStringConsumer {
+    Logger logger = LoggerFactory.getLogger(KafkaStringConsumer.class);
+
+    @KafkaListener(topics = "TOPIC-DEMO",groupId = "group_id")
+    public void consume(String message){
+        logger.info("Consuming Message {}",message);
+    }
+}
